fa2df7b242caad064a3630076effd30d9adb303e
21lucasmessias <21.lucasmessias@gmail.com>
2021-06-22 15:47:42 -0300

    Chapter 1 - Lesson 10 Print and val/var

--- Kotlin_1/src/main/kotlin/main.kt
+++ Kotlin_1/src/main/kotlin/main.kt
@@ -1,5 +1,8 @@
 fun main() {
     val titular = "Lucas"
+    val conta = 1234
+    var saldo = 100.0
+    saldo += 200
 
-    print("Bem vindo ao Bytebank\ntitular $titular")
+    print("Bem vindo ao Bytebank\ntitular $titular\nconta $conta\nsaldo $saldo")
 }
